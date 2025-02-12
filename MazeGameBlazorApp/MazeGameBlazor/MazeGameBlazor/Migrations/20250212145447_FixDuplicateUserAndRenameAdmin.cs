f21edb38a032cf0351a427ea4ff1297fc26a37f1
Alexandre Emond <143522099+a3emond@users.noreply.github.com>
2025-02-12 11:13:19 -0500

    improved Styling pre implemented game page

---
+++ MazeGameBlazorApp/MazeGameBlazor/MazeGameBlazor/Migrations/20250212145447_FixDuplicateUserAndRenameAdmin.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MazeGameBlazor.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixDuplicateUserAndRenameAdmin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // 1️Remove the duplicate "ALEXEMOND" user
+            migrationBuilder.Sql(
+                "DELETE FROM AspNetUsers WHERE NormalizedUserName = 'ALEXEMOND';"
+            );
+
+            // 2️Update "ADMIN" username to "AlexEmond"
+            migrationBuilder.Sql(
+                "UPDATE AspNetUsers " +
+                "SET UserName = 'AlexEmond', NormalizedUserName = 'ALEXEMOND' " +
+                "WHERE NormalizedUserName = 'ADMIN';"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
